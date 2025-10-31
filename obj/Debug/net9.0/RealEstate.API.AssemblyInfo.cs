5340159a226ee305b72407e02da649a3c58526e3
jlmunozpb <jlmunozpb@gmail.com>
2025-10-31 12:15:35 -0500

    docker

--- obj/Debug/net9.0/RealEstate.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/RealEstate.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1473d062fa407ec84812d18ee8f591041e0d2da2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128f520d2e32a0464ec3fd26a67652e0f067799e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
