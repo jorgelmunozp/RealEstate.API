96c1c076184dd2fcf72bc4b8ca8d48f9943e7332
jlmunozpb <jlmunozpb@gmail.com>
2025-10-30 19:17:52 -0500

    deploy

--- obj/Debug/net9.0/RealEstate.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/RealEstate.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0222cf22b13fa7249e66a82c57f96fc767261048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53b3676849bd5dcc41377f2c19d5aeee35b5aa0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
